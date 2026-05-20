813cbc20dbca9a60d3be276dd90c32d768be87f9
PhiliposHailu <hailuphilipos@gmail.com>
2026-05-20 22:01:38 +0300

    update: update and flutter understanding

--- lib/main.dart
+++ lib/main.dart
@@ -4,9 +4,6 @@ import 'package:flutter_riverpod/flutter_riverpod.dart';
 
 void main() {
   runApp(
-    // THE WHY: ProviderScope is the "brain" of Riverpod. 
-    // By wrapping our entire app in it, any screen or widget 
-    // inside our app can easily talk to our database or AI logic.
     const ProviderScope(
       child: BirrNoteApp(),
     ),
@@ -21,9 +18,6 @@ class BirrNoteApp extends StatelessWidget {
     return MaterialApp(
       title: 'BirrNote',
       debugShowCheckedModeBanner: false,
-      // THE WHY: Material 3 is Google's latest design system. 
-      // It gives us beautiful, modern UI components out of the box.
-      // We are using a deep green seed color since this is a finance app!
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(seedColor: const Color(0xFF1E88E5)), // We can tweak this later!
         useMaterial3: true,
