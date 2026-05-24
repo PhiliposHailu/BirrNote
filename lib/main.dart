2b4160400d63ea16f6e678a27f83f7828cf8ed4a
PhiliposHailu <hailuphilipos@gmail.com>
2026-05-24 19:07:48 +0300

    minor update

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,6 @@
 import 'features/expense_entry/presentation/expense_entry_screen.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
-
 void main() {
   runApp(
     const ProviderScope(
